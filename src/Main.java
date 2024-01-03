94204b8c33c3c6afb9b7dbaa45e19bc935f72945
Chris <chris.tasche@mtec-cc.com>
2024-01-03 10:34:02 +0100

    [F] Prozent Augenzähler

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,6 @@
 public class Main
 {
     public static void main(String[] args) {
-        //Worker w = new Worker();
-        //w.howManyNonStatic();
-        //Worker.howMany();
-        Wuerfel w1 = new Wuerfel();
-        Wuerfel w2 = new Wuerfel();
-        //Worker.paschKurz(w1.wurf(), w2.wurf());
-        //Worker.bisZumPaschUndDarueberHinaus();
-        Worker.augenzaehler();
+        Worker.augenProzent(200);
     }
 }
--- src/Worker.java
+++ src/Worker.java
@@ -1,5 +1,6 @@
 public class Worker {
     private static final int[] augen = new int[6];
+
     public static void howMany(){
         Wuerfel w = new Wuerfel();
         int anzahl6 = 0;
@@ -23,21 +24,25 @@ public class Worker {
         }
     }
 
-    public static void augenzaehler(){
+    public static void augenzaehlerBisHundert(){
         Wuerfel w = new Wuerfel();
         for (int i = 1; i <= 100; i++){
+            // w.wurf() ergibt eine Zahl von 1 bis 6
+            // Ergebnis der Würfe einer 1 werden in Index 0
+            // u.s.w. gespeichert. Daher Ergebnis des Wurfs -1
             augen[w.wurf() -1]++;
         }
         int augenKey = 1;
         for (int auge : augen){
             System.out.println("Anzahl der Würfe einer " +augenKey+ ": " + auge);
+            augenKey++;
         }
     }
+
     public static void paschKurz(int wurf1, int wurf2){
         //ternary / ternärer Operator
         System.out.println(wurf1 == wurf2 ? "Pasch" : "Kein Pasch!");
     }
-
     public static boolean paschBool(int wurf1, int wurf2){
         return wurf1 == wurf2;
     }
@@ -45,32 +50,32 @@ public class Worker {
     public static void bisZumPaschUndDarueberHinaus(){
         Wuerfel w1 = new Wuerfel();
         Wuerfel w2 = new Wuerfel();
-        int wuerfe = 0;
+        int paschWuerfe = 0;
         int erfolglos = 0;
         boolean erg = false;
-        while (wuerfe == 0) {
+        while (paschWuerfe == 0) {
 
             do {
                 erg = Worker.paschBool(w1.wurf(), w2.wurf());
-                if (erg) wuerfe++;
+                if (erg) paschWuerfe++;
             } while (erg);
 
             erfolglos++;
         }
-        System.out.println("Nach " + erfolglos + " Versuchen gab es einen Pasch - und dann direkt " + wuerfe + " hintereinander.");
+        System.out.println("Nach " + erfolglos + " Versuchen gab es einen Pasch - und dann direkt " + paschWuerfe + " hintereinander.");
     }
-    public void howManyNonStatic(){
+
+    public static void augenProzent(int count)
+    {
         Wuerfel w = new Wuerfel();
-        int anzahl6 = 0;
-        int anzahlNicht6 = 0;
-        for (int i = 1; i <= 100; i++){
-            int erg = w.wurf();
-            if (erg == 6){
-                anzahl6++;
-            } else {
-                anzahlNicht6++;
-            }
+        for (int i = 1; i <= count; i++){
+            augen[w.wurf() -1]++;
         }
-        System.out.printf("Gewürfelte 6er: %d. %nNicht 6er: %d", anzahl6, anzahlNicht6);
+        int augenKey = 1;
+        for (int auge : augen){
+            double prozent = ((double)auge / count) * 100;
+            System.out.print("Anzahl der Würfe einer " +augenKey+ ": ");
+            System.out.print(auge + "(" + Math.round((prozent *100)) /100 + "%)\n ");
+        }
     }
 }
